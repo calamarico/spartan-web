069688cf19ffa702ba9d0f6e68a56b0ec786ca92
Daniel Hernandez Zafra <daniel.hernandez.zafra@gmail.com>
2015-11-06 13:27:54 +0100

    CHANGES in app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-  var ESPARTANO = '.espartano',
-    CIRCULO = '.circulo';
+var ESPARTANO = '.espartano',
+  CIRCULO = '.circulo';
 
-    move(ESPARTANO)
-      .translate(300, -16)
-      .ease('in')
+function downSpartan(callback) {
+  move(ESPARTANO)
+    .translate(300, -16)
+    .ease('in')
       .duration('0.4s')
-      .end();
+      .end(callback);
+}
+
+$(document).ready(function() {
+  downSpartan();
 });
