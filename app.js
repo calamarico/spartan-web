41c6487691e968fcb780c592fc42730bf2bcfcb0
Daniel Hernandez Zafra <daniel.hernandez.zafra@gmail.com>
2015-12-03 16:34:38 +0100

    last changes

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var ESPARTANO = '.espartano',
   CIRCULO = '.circulo';
 
+function detectIE() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf('MSIE ');
+  var trident = ua.indexOf('Trident/');
+  var edge = ua.indexOf('Edge/');
+
+  if (msie > 0 || trident > 0 || edge > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function downSpartan(callback) {
   move(ESPARTANO)
-    .translate(300, -16)
+    .translate(300, -18)
     .ease('in')
       .duration('0.4s')
       .end(callback);
 }
 
+function showEye() {
+  $('#eye').css('display', 'inherit');
+}
+
 $(document).ready(function() {
-  downSpartan();
+  $('#circulo').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd ' +
+    ' oanimationend animationend', function(param) {
+      downSpartan(detectIE() ? undefined : showEye);
+    });
+  $('#circulo').addClass('animated fadeIn');
 });
